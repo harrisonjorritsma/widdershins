dad89ba1feba57590058baac0cb62e25eec13d80
Mike Ralphson <mike.ralphson@gmail.com>
2018-01-10 16:59:31 +0000

    new options; html, noHeader, respec

--- openapi3.js
+++ openapi3.js
@@ -535,9 +535,9 @@ function convertInner(api, options, callback) {
         return s.split('\r').join('').split('\n').join(' ').trim();
     };
 
-    let content = '---\n'+yaml.dump(header)+'\n---\n\n';
-        data = options.templateCallback('main', 'pre', data);
-        if (data.append) { content += data.append; delete data.append; }
+    let content = options.noHeader ? '' : '---\n'+yaml.dump(header)+'\n---\n\n';
+    data = options.templateCallback('main', 'pre', data);
+    if (data.append) { content += data.append; delete data.append; }
     try {
         content += templates.main(data);
     }
@@ -547,6 +547,8 @@ function convertInner(api, options, callback) {
     data = options.templateCallback('main', 'post', data);
     if (data.append) { content += data.append; delete data.append; }
     content = content.replace(/^\s*[\r\n]/gm,'\n\n'); // remove dupe blank lines
+
+    if (options.html) content = common.html(content,header,options);
 
     callback(null,content);
 }
